a5fcc9fd7a86db22c59f38250258d3b5cc1e2697
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-08 10:56:28 -0700

    no port

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.get("/random/glowhosttest", (req, res) => {
   res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
   res.end("Hey");
-}).listen(PORT);
+}).listen();
