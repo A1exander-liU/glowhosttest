ba2ba1bc673a38c3d4d70673f63cde0cc25d93ee
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-08 11:22:04 -0700

    update

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.get("/random/glowhosttest", (req, res) => {
   res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
-  res.end("Hey");
+  res.send("Hey");
 }).listen();
