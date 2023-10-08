39e4aae53b828077d4d8247fad9a5e1fe2d0c2fa
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-08 10:40:09 -0700

    add cors

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
+  res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
   res.end("Hey");
 }).listen(PORT);
