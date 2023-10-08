6ba8d44c66eebe3ae67bf48d0b62d7d144305ecf
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-08 10:55:30 -0700

    update root path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+app.get("/random/glowhosttest", (req, res) => {
   res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
   res.end("Hey");
 }).listen(PORT);
