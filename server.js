8e3e95793f306276c5067d92f4cff004e0a63557
AlexanderLiu <alexanderliu2016@gmail.com>
2023-10-08 11:30:13 -0700

    remove headers

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ const express = require("express");
 const app = express();
 
 app.get("/random/glowhosttest", (req, res) => {
-  res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
   res.send("Hey");
 }).listen();
